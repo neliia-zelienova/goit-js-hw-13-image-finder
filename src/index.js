820c30aef5e5bcbb5ddf1488d5bea96d74ed7772
neliia-zelienova <66479463+neliia-zelienova@users.noreply.github.com>
2021-01-03 00:21:48 +0200

    добавляет разметку галереи, формы поиска, функционал открытия модального окна

--- src/index.js
+++ src/index.js
@@ -8,8 +8,71 @@ formRef.addEventListener('submit', (event) => event.preventDefault());
 
 const inputRef = formRef.querySelector('input');
 
-inputRef.addEventListener('input', debounce(() => {
-    console.log(inputRef.value);
-    render();
-    }, 500)
-);
+// inputRef.addEventListener('input', debounce(() => {
+    
+//     render();
+//     }, 500)
+// );
+
+
+render();
+
+const lightBoxAction = (action) => {
+    const parentDivRef = document.querySelector('.lightbox');
+    if (action === 'open') {
+         if (!(parentDivRef.classList.contains('is-open'))) {
+            parentDivRef.classList.add('is-open');
+        }
+    } else if (action === 'close') {
+        resetLightBoxImg();
+        if (parentDivRef.classList.contains('is-open')) {
+            parentDivRef.classList.remove('is-open');
+        }
+    }
+}
+
+const getImageData = (target) => {
+    return [target.dataset.source, target.alt];
+};
+
+const openImage = (elem) => {
+    elem.preventDefault();
+    const LightboxRef = document.querySelector('.lightbox');
+    if (LightboxRef) {
+        lightBoxAction('open');
+    }
+    currentImageIndex = elem.target.dataset.index;
+    updLightBoxImg (...getImageData(elem.target));
+}
+
+const openImageByIndex = (index, images) => {
+    if (index >= 0 && index < images.length) {
+        updLightBoxImg(images[Number(index)].original, images[Number(index)].description);
+    }
+}
+
+const updLightBoxImg = (src, alt) => {  
+    const lightBoxImgRef = document.querySelector('.lightbox__image');
+    lightBoxImgRef.src = '';
+    lightBoxImgRef.alt = '';
+    if (src) lightBoxImgRef.src = src;
+    if (alt) lightBoxImgRef.alt = alt;   
+}
+
+const resetLightBoxImg = () => {
+    updLightBoxImg();
+}
+
+const lightBoxHandler = (event) => {
+    if (event.target.nodeName === 'BUTTON' || event.target.classList.contains('lightbox__overlay')) { 
+        lightBoxAction('close');
+    }
+}
+
+
+const ulRef = document.querySelector('.gallery');
+
+ulRef.addEventListener('click', (event) => {
+    openImage(event);
+    console.log('click');
+});
