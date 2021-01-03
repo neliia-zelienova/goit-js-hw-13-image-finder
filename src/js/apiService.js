6ce656252280b337b8821c3ec6a1fbb03188f993
neliia-zelienova <66479463+neliia-zelienova@users.noreply.github.com>
2021-01-03 21:08:53 +0200

    добавляет код запроса на сервер для получения картинок (только первую страницу на 12 изображений) и рендерит их

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,6 +1,21 @@
 import axios from 'axios';
+import { render } from './render';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
 
 const myAPIkey = '19701765-66040315454630f125fe17080';
 
+
+//https://pixabay.com/api/?image_type=photo&orientation=horizontal
+//& q=что_искать & page=номер_страницы & per_page=12 & key=твой_ключ
+
+export const getImages = (searchTerm, pageNumber) => {
+
+    
+    const Url = `&q=${searchTerm.replace(' ', '+')}&page=${pageNumber}&per_page=12&key=${myAPIkey}`;
+    axios.get(Url).then(data => {
+        console.log('in axios', data.data.hits);
+        render(data.data.hits);
+    });
+
+};
